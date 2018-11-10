7c5847c04441ef67443129397314e02d93188265
munniomer <munniomer@gmail.com>
2018-11-10 14:08:10 +0300

    [feature  #161840741] registered the signup resource

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,12 @@ from config import app_config
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.url_map.strict_slashes = False
     app.config.from_object(app_config[config_name])
+
+    from app.api.v1 import v1
+    app.register_blueprint(v1)
+
+
 
     return app
