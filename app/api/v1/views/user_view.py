36505039b8879ef68d9bb1b1f4de0edebd72ea41
munniomer <munniomer@gmail.com>
2018-11-12 07:03:53 +0300

    [finishes #161840741] added tests for the user registration endpoint

--- app/api/v1/views/user_view.py
+++ app/api/v1/views/user_view.py
@@ -15,6 +15,7 @@ class SignupResource(Resource):
     def post(self):
         """Method for posting user data"""
         request_data = request.get_json()
+        print(request_data)
         fname = request_data["fname"]
         lname = request_data["lname"]
         email = request_data["email"]
@@ -22,24 +23,6 @@ class SignupResource(Resource):
         password = request_data["password"]
         confirm_password = request_data["confirm_password"]
         city = request_data["city"]
-
-        if not validate.valid_name(fname):
-            return {'message': "fname cant be empty and should only contain letters"}, 400
-        
-        if not validate.valid_name(lname):
-            return {'message': "lname cant be empty and should only contain letters"}, 400
-        
-        if not validate.valid_email(email):
-            return {'message': "please Enter a valid email"}, 400
-        
-        if not validate.valid_password(password):
-            return {'message': "password cant be empty and not less than 3"}, 400
-
-        if not validate.valid_name(city):
-            return {'message': "please Enter a valid city"}, 400
-        
-        if not isinstance(phone,int):
-            return {'message': "please Enter a valid phone number"}, 400
 
         db.add_user(fname, lname, email, phone, password, confirm_password,city)
         
