8ba1445fc14fae172843ed4fde0d6baaa599c264
munniomer <munniomer@gmail.com>
2018-11-14 22:17:44 +0300

    [finishes #161963319] registers the get specific parcel endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource 
 from app.api.v1.views.user_view import SignupResource
-from app.api.v1.views.parcels_view import ParcelResource
+from app.api.v1.views.parcels_view import ParcelResource,ParcelSpecific
+
 
 v1 = Blueprint('apiv1', __name__, url_prefix='/api/v1')
 app = Api(v1)  
@@ -12,5 +13,7 @@ app = Api(v1)
 app.add_resource(SignupResource, '/user/register')
 
 # Parcels
-app.add_resource(ParcelResource, '/Parcels')
+app.add_resource(ParcelResource, '/parcels')
+app.add_resource(ParcelSpecific, '/parcels/<int:parcel_Id>')
+
 
